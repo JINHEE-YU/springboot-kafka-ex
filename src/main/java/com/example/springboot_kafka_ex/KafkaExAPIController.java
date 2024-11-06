85376d9ca4876f86cf545a9554c8da346cc1b6ad
JINHEE-YU <yujh2292@gmail.com>
2024-11-06 18:35:23 +0900

    GET,POST 추가

--- src/main/java/com/example/springboot_kafka_ex/KafkaExAPIController.java
+++ src/main/java/com/example/springboot_kafka_ex/KafkaExAPIController.java
@@ -1,6 +1,9 @@
 package com.example.springboot_kafka_ex;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springboot_kafka_ex.producer.KafkaProducer;
@@ -29,12 +32,40 @@ public class KafkaExAPIController {
     return "hello";
   }
 
+  @GetMapping("/send/msg")
+  public String sendMessage(@RequestParam(value = "message") String message) {
+
+    String topic = "topic1";
+
+    try {
+      kafkaProducer.sendMessage(topic, message);
+      return "success";
+    } catch (Exception e) {
+      log.error("오류발생", e);
+      return "failure";
+    }
+  }
+
   @GetMapping("/send2")
   public String sendMessage2() {
 
     String topic = "topic2";
 
     UserDTO user = UserDTO.builder().age(10).nickName("Mika").chatMsg("I'm Mika").build();
+    try {
+      kafkaProducer.sendMessage(topic, user);
+      return "success";
+    } catch (Exception e) {
+      log.error("오류발생", e);
+      return "failure";
+    }
+  }
+
+  @PostMapping("/send2/user")
+  public String sendUserDTO(@RequestBody UserDTO user) {
+
+    String topic = "topic2";
+
     try {
       kafkaProducer.sendMessage(topic, user);
     } catch (Exception e) {
