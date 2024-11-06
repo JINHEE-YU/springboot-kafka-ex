e781acaee474834853dfc4198476f4c18cb96cf9
JINHEE-YU <yujh2292@gmail.com>
2024-11-06 15:35:43 +0900

    first commit

---
+++ src/main/java/com/example/springboot_kafka_ex/SpringbootKafkaExApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_kafka_ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_kafka_ex/SpringbootKafkaExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_kafka_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
