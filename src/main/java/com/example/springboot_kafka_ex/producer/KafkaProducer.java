d050c41b0e9dda659527f9c913be94173dce5c99
JINHEE-YU <yujh2292@gmail.com>
2024-11-06 17:41:26 +0900

    KafkaProducer.java 코드 개선

--- src/main/java/com/example/springboot_kafka_ex/producer/KafkaProducer.java
+++ src/main/java/com/example/springboot_kafka_ex/producer/KafkaProducer.java
@@ -12,19 +12,29 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 public class KafkaProducer {
-
   private final KafkaTemplate<String, String> kafkaTemplate;
   private final KafkaTemplate<String, UserDTO> kafkaTemplate2;
 
   public void sendMessage(String topic, String message) {
-    log.info("call send message");
-    kafkaTemplate.send(topic, message);
-    log.info("Kafka Producer send data = {} / topic ={}", message, topic);
+    sendMessage(kafkaTemplate, topic, message);
   }
 
   public void sendMessage(String topic, UserDTO message) {
-    log.info("call send message");
-    kafkaTemplate2.send(topic, message);
-    log.info("Kafka Producer send data = {} / topic ={}", message, topic);
+    sendMessage(kafkaTemplate2, topic, message);
+  }
+
+  private <T> void sendMessage(KafkaTemplate<String, T> tpl, String topic, T message) {
+    try {
+      log.info("Sending message to topic: {}", topic);
+      tpl.send(topic, message).whenComplete((result, ex) -> {
+        if (ex != null) {
+          log.error("Failed to send message = {} to topic = {} due to {}", message, topic, ex.getMessage());
+        } else {
+          log.info("Kafka Producer sent data = {} to topic = {}", message, topic);
+        }
+      });
+    } catch (Exception e) {
+      log.error("Exception while sending message = {} to topic = {}", message, topic, e);
+    }
   }
 }
